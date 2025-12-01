4c38aece17577015f682478673be279060dcb914
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2025-12-01 20:15:05 -0300

    Enhance app layout with font loading and splash screen handling; update index component for offer display; refine global styles and package configurations.

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native';
+import { offers } from '@/constants';
+import { Fragment } from 'react';
+import { FlatList, Image, Pressable, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import cn from 'clsx';
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      {' '}
-      <Text className="text-xl font-bold text-blue-500">Deu certo!</Text>{' '}
-    </View>
+    <SafeAreaView className="">
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+
+          return (
+            <View>
+              <Pressable className={cn()} style={{ backgroundColor: item.color }}>
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className="h-full w-1/2">
+                      <Image source={item.image} className={`size-full`} resizeMode="contain" />
+                    </View>
+
+                    <View className={`offer-card__info`}>
+                      <Text>{item.title}</Text>
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
