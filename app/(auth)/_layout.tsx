b39a4f93b20c56f2d432a408037718e9aea06b15
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2025-12-05 21:38:06 -0300

    Refactor layout to include custom input and button components; enhance UI with image backgrounds and scrollable view

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,43 @@
-import { Text } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from 'react-native';
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Slot } from 'expo-router';
+import { images } from '@/constants';
+import CustomInput from '@/components/CustomInput';
+import CustomButton from '@/components/CustomButton';
 
 export default function Layout() {
   return (
-    <SafeAreaView>
-      <Text>Layout</Text>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView className="h-full bg-white" keyboardShouldPersistTaps="handled">
+        <View
+          className="relative w-full"
+          style={{ height: Dimensions.get('screen').height / 2.35 }}>
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image source={images.logo} className="absolute -bottom-16 z-10 size-48 self-center" />
+        </View>
+
+        <CustomInput
+          placeholder="Enter your email"
+          value={''}
+          onChangeText={(text) => {}}
+          label="Email"
+          keyboardType="email-address"
+        />
+        <CustomButton />
+      </ScrollView>
       <Slot />
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
