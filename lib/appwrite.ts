c980c09228602bea676ad115bd86eb82da77b20f
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2025-12-19 21:35:55 -0300

    Refactor Search component to use SafeAreaView; update appwrite.ts to include getMenu function for querying menus; modify seed.ts to set default image type for uploaded files.

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { CreateUserParams, SignInParams } from '@/type';
+import { CreateUserParams, SignInParams, GetMenuParams } from '@/type';
 import { Account, Avatars, Client, Databases, ID, Query, Storage } from 'react-native-appwrite';
 
 export const appwriteConfig = {
@@ -86,6 +86,25 @@ export const getCurrentUser = async () => {
     if (!currentUser) throw Error;
 
     return currentUser.documents[0];
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+export const getMenu = async ({ category, query }: GetMenuParams) => {
+  try {
+    const queries: string[] = [];
+
+    if (category) queries.push(Query.equal('categories', category));
+    if (query) queries.push(Query.search('name', query));
+
+    const menus = await databases.listDocuments({
+      databaseId: appwriteConfig.databaseId!,
+      collectionId: appwriteConfig.menuCollectionId,
+      queries: queries,
+    });
+
+    return menus.documents;
   } catch (error) {
     throw new Error(error as string);
   }
